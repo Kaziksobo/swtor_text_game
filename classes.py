5a4571fa0f381cffb545922cc67621932f4d7dfd
Kazik Sobotnicki <kaziksobo@outlook.com>
2023-06-28 04:14:03 +0100

    Added classes file

---
+++ classes.py
@@ -0,0 +1,9 @@
+class classes:
+    def __init__(self, armour: str, weapon: str, power: str, attack: int, defense: int, stealth: int, force: int):
+        self.armour = armour
+        self.weapon = weapon
+        self.power = power
+        self.attack = attack
+        self.defense = defense
+        self.stealth = stealth
+        self.force = force
